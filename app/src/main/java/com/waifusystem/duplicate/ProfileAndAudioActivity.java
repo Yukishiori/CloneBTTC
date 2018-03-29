fcf5fdf4e6a9463f3aef701f54cfa225bca45327
YukiShiori <anmana000@gmail.com>
2018-03-29 15:42:08 +0700

    fix playerIntent null bug

--- app/src/main/java/com/waifusystem/duplicate/ProfileAndAudioActivity.java
+++ app/src/main/java/com/waifusystem/duplicate/ProfileAndAudioActivity.java
@@ -135,11 +135,9 @@ public class ProfileAndAudioActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        stopService(playerIntent);
+        if (playerIntent != null) {
+            stopService(playerIntent);
+        }
     }
 
-//    @Override
-    public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "china ?", Toast.LENGTH_SHORT).show();
-    }
 }
